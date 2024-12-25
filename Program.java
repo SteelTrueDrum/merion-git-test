1d95c0aa2588d6bc87d356e38e3c4f2c10e6a6dd
SteelTrueDrum <steeltruedrum@gmail.com>
2024-12-25 13:11:37 +0300

    first commit

---
+++ Program.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("First Easy Manual Java Project from Notepad");
+		System.out.println(Arrays.toString(args));
+		Scanner scanner = new Scanner(System.in);
+		String answer = scanner.nextLine();
+		System.out.println("Вы ввели: " + answer);
+
+	}
+
+}
